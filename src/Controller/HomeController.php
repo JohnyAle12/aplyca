9de4a342df2dc54c6667c409220a679f4da50b09
Johny Prieto <johnyprieto001@gmail.com>
2021-06-25 23:47:39 -0500

    Feat: configuración modelo de usuario y formularios de autenticación

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/home", name="home")
-     */
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
